17275360ad8743a3e953529c04c74f10a89e7578
r4ph433l <70282135+r4ph433l@users.noreply.github.com>
2024-07-03 16:31:17 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 // Hello.java
-public class Hello{
+class Hello{
 	public static void main(String... args){
-		//int[] msg = (int[]) "Hello World!";
-		//System.out.println((String) msg);
-		System.out.println("Hello World!");
+		System.out.println("Hello World" + "!".repeat(3));
 	}
 }
