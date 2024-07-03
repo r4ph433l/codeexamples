6aa8cc4bfbe12f15afb933432c8aad9c20821d75
r4ph433l <70282135+r4ph433l@users.noreply.github.com>
2024-07-03 16:34:05 +0200

    Update hello.s

--- hello.s
+++ hello.s
@@ -2,8 +2,10 @@
 global _start
 
 section .data
-	msg: db "Hello world!", 10
+	msg:    db “Hello World”
 	msglen: equ $ - msg
+	exclam: db “!”
+	n:      db 3
 
 section .text
 
@@ -13,6 +15,23 @@ _start:
 	mov	rsi, msg
 	mov	rdx, msglen
 	syscall
+
+ 	mov	rcx, [n]
+l1:
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, exclam
+	mov	rdx, 1
+	push	rcx
+	syscall
+	pop	rcx
+	loop	l1
+
+	mov	rax, 1
+ 	mov	rdi, 1
+  	mov	rsi, 10
+   	mov	rdx, 1
+    	syscall
 
 	mov	rax, 60
 	mov	rdi, 0
